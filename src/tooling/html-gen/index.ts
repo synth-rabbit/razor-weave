a7f91c8832d667fc5b5cbd127099e956530de58d
Josh 'Panda' Edwards <josh.dean.edwards@gmail.com>
2025-11-21 08:53:15 -0600

    feat(html-gen): create module structure

---
+++ src/tooling/html-gen/index.ts
@@ -0,0 +1,8 @@
+/**
+ * HTML Generation Module
+ *
+ * Shared infrastructure for generating HTML from markdown.
+ * Used by both print-design and web-reader workflows.
+ */
+
+// Utilities - exports added as modules are implemented
